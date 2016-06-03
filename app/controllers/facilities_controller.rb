67c6c02f822ef85c86f77717abeb53715a424bdc
Antony Siegert <agsiegert@hotmail.com>
2016-06-03 09:08:34 -0600

    facility widgets working (headline and text)

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,6 +1,5 @@
 class FacilitiesController < ApplicationController
   include Scrivito::ControllerHelper
-
   before_action :fetch_scrivito_object, only: [:show]
 
   def index
@@ -13,19 +12,24 @@ class FacilitiesController < ApplicationController
   end
 
   def create_scrivito_obj
+    set_facility
     FacilityData.create_for(@facility)
     redirect_to facility_path(@facility)
   end
 
   private
 
-    def fetch_scrivito_object
-      @obj =
-        begin
-          FacilityData.find_for(@facility)
-        rescue Scrivito::ResourceNotFound
-          # Nothing to do here, handled in template
-        end
-    end
+  def set_facility
+    @facility = Facility.find(params[:id])
+  end
 
+  def fetch_scrivito_object
+    set_facility
+    @obj =
+      begin
+        FacilityData.find_for(@facility)
+      rescue Scrivito::ResourceNotFound
+        # Nothing to do here, handled in template
+      end
+  end
 end
